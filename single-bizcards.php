96c4cca774ca768b4eb5c3511d011062ef4fa444
Kristin Yetter <kristin@dameranchdesigns.com>
2017-09-15 16:47:31 -0600

    make drdcard
    added CRUD functionality for drdcard

--- single-bizcards.php
+++ single-bizcards.php
@@ -20,44 +20,10 @@ get_header('bizcards'); ?>
 		?>
 
 		<?php
-		endwhile; // End of the loop. ?>
+		endwhile; // End of the loop.
 
-		<pre>
-			<?php
-			global $current_user;
-			echo DAISY_INC_URL."<br>";
-			echo DAISY_INC_PATH."<br>";
 
-			if(isset($_POST['post_id'])){
-				print_r(get_post($_POST['post_id']));
-			}
-      echo "----------------POST-----------------<br>";
-      print_r($_POST);
-
-      echo "----------------FILES-----------------<br>";
-			print_r($_FILES);
-
-      echo "----------------errors-----------------<br>";
-    	print_r($errors);
-
-      echo "----------------session-----------------<br>";
-			print_r($_SESSION);
-			unset($_SESSION['complete']);
-			unset($_SESSION['vcard-set-raw']);
-			unset($_SESSION['vcard_post_is_set']);
-			unset($_SESSION['child']);
-			unset($_SESSION['org-vcard']);
-			unset($_SESSION['message']);
-
-			echo "post_meta".get_post_meta(33, '_wp_attached_file', true);
-
-      echo "<br>";
-      print_r(get_allowed_mime_types());
-      echo "<br>";
-			echo $pagenow;
-
-			 ?>
-		</pre>
+		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- templates/new_bizcard.php
+++ templates/new_bizcard.php
@@ -33,39 +33,6 @@
  		<?php
  		endwhile; // End of the loop. ?>
 
- 		<pre>
- 			<?php
- 			global $current_user;
-
- 			if(isset($_POST['post_id'])){
- 				print_r(get_post($_POST['post_id']));
- 			}
-       echo "----------------POST-----------------<br>";
-       print_r($_POST);
-
-       echo "----------------FILES-----------------<br>";
- 			print_r($_FILES);
-
-       echo "----------------errors-----------------<br>";
-     	print_r($errors);
-
-       echo "----------------session-----------------<br>";
- 			print_r($_SESSION);
- 			unset($_SESSION['complete']);
- 			unset($_SESSION['vcard-set-raw']);
- 			unset($_SESSION['vcard_post_is_set']);
- 			unset($_SESSION['child']);
- 			unset($_SESSION['org-vcard']);
- 			unset($_SESSION['message']);
-
- 			echo "post_meta".get_post_meta(33, '_wp_attached_file', true);
-
-       echo "<br>";
-       print_r(get_allowed_mime_types());
-       echo "<br>";
-
- 			 ?>
- 		</pre>
 
  		</main><!-- #main -->
  	</div><!-- #primary -->
